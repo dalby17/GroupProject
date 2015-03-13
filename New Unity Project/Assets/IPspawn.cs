462b6f9f0bfed9c1da97e378048c13a0e6a37a91
SQ88 <yakato@hotmail.co.uk>
2015-03-13 11:23:49 +0000

    added unity project

---
+++ New Unity Project/Assets/IPspawn.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IPspawn : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		string[] ipAddresses = new string[100];
+		string address = "";
+		for (int i = 0; i < 100; i++) {
+			for (int x = 0; x < 4; x++) {
+				int three = Random.Range(0, 1000);
+				string threeGroup = three.ToString();
+				if (three < 100 && three >= 10) {
+					threeGroup = threeGroup + "0";
+				}
+				else if (three < 10) {
+					threeGroup = threeGroup + "00";
+				}
+				if (x < 3) {
+					address = address + threeGroup + ".";
+				}
+				else {
+					address = address + threeGroup;;
+				}
+			}
+			ipAddresses[i] = address;
+			Debug.Log(address);
+			address = "";
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
---
+++ New Unity Project/Assets/Spawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject box;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("spawn", 2.0f, 2.0f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void spawn() {
+
+		Instantiate(box, new Vector3(0, 0, 2), Quaternion.identity);
+	}
+}
