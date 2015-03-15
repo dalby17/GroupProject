8d9fffdedd30109f28dabd44df91161dccf1440a
GFWatson <gabriellewatson46@yahoo.co.uk>
2015-03-15 14:38:02 +0000

    Read description!
    1) anything you add make sure it's saved as an asset because I had to
    remake anything that wasn't. 2) when you load this up now add
    mainCamera, level01 and plane into the scene. 3) I've made it so we have
    3 boxes that scroll down the screen and destroy themselves.

---
+++ GroupProject/Assets/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject box;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("spawn", 2.0f, 2.0f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void spawn() {
+
+		Instantiate(box, new Vector3(-28, 30, 49), Quaternion.identity);
+		Instantiate(box, new Vector3(-9, 30, 49), Quaternion.identity);
+		Instantiate(box, new Vector3(10, 30, 49), Quaternion.identity);
+	}
+}
---
+++ GroupProject/Assets/Translate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Translate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveSpeed = 5f;
+		transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+		if (transform.position.y <= -23) {
+			Destroy (gameObject);
+		}
+	}
+}
