18c3a3caa2c25914582d6abbd2e163e596752dc4
cpthooke <stevenhooke@hotmail.co.uk>
2015-03-20 16:18:28 +0000

    File Size script
    So I've been fiddling with rotation and such to find rotation on GUI
    elements (such as text boxes) is very different to that of 3D rotation
    so I modified the IP text box script to make something similar for file
    size. I imagine I can add to with other things we need to check for (for
    different levels) but cross that bridge when we get there. What I was
    thinking is instead of right-clicking and rotating was maybe just
    rolling the mouse over the text and getting a popup box. The idea for it
    is here:
    http://answers.unity3d.com/questions/17812/text-box-pop-up-on-mouseover.html
    If you want to have a go, go ahead. If not upload a more up-to-date
    version of the game and I'll have a go.

---
+++ GroupProject/Assets/IPspawnV2.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IPspawnV2 : MonoBehaviour {
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		//string[] ipAddresses = new string[100];
+		//string address = "";
+		//for (int i = 0; i < 100; i++) {
+		//	for (int x = 0; x < 4; x++) {
+		//		int three = Random.Range(0, 1000);
+		//		string threeGroup = three.ToString();
+		//		if (three < 100 && three >= 10) {
+		//			threeGroup = threeGroup + "0";
+		//		}
+		//		else if (three < 10) {
+		//			threeGroup = threeGroup + "00";
+		//		}
+		//		if (x < 3) {
+		//			address = address + threeGroup + ".";
+		//		}
+		//		else {
+		//			address = address + threeGroup;;
+		//		}
+		//	}
+		//	ipAddresses[i] = address;
+		//	Debug.Log(address);
+		//	address = "";
+		//}
+		
+		
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButton(1))
+			OnRightClick();
+	}
+	
+	// Update is called once per frame
+	void OnRightClick () {
+		string [] fileArray = new string[] {"kB", "kB", "kB", "kB", "kB", "kB", "kB", "kB", "gB", "gB"}; 
+		string[] otherInfo = new string[100];
+		string info = "";
+		//if (Input.GetMouseButton(1))
+		//{
+		Ray clickPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitPoint;
+		
+		// See if the ray collided with an object
+		if (Physics.Raycast(clickPoint, out hitPoint))
+		{
+			// Make sure this object was the
+			// one that received the right-click
+			if (hitPoint.collider == this.GetComponent<Collider>())
+			{
+			for (int j = 0; j< 100; j++)
+			{
+				int capacity = Random.Range(0,11);
+				string capacityString = capacity.ToString();
+				string fileType = fileArray[Random.Range(0,fileArray.Length-1)];
+				otherInfo[j] = capacityString + fileType;
+				Debug.Log(capacityString + fileType);
+				capacityString = "";
+				fileType = "";
+			}
+			}
+		}
+		//}
+		
+	}
+}
